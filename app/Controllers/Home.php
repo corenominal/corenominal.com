95b72050bb3398c82b69c4e7f605ba9334775a87
Philip Newborough <corenominal@corenominal.org>
2026-06-05 13:49:21 +0100

    Refactor index method in Home controller to include user check and redirect

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,18 @@
 
 namespace App\Controllers;
 
+use Modules\Auth\Models\UserModel;
+
 class Home extends BaseController
 {
-    public function index(): string
+    public function index()
     {
+        // Check if there are any users in the database, and if not, redirect to the login page to encourage setup.
+        $userModel = new UserModel();
+        if ($userModel->countAllResults() === 0) {
+            return redirect()->to('/auth/register');
+        }
+    
         $data['title'] = 'Under Construction';
         return view('under-construction', $data);
     }
