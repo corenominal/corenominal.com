ae97834ce98e23e6f324dd9f137ab75046dafb33
Philip Newborough <corenominal@corenominal.org>
2026-06-05 13:07:01 +0100

    Update image class in under construction page for consistency

--- app/Views/under-construction.php
+++ app/Views/under-construction.php
@@ -1,7 +1,7 @@
 <?= $this->extend('templates/basic-centered') ?>
 <?= $this->section('content') ?>
 
-    <img src="/assets/img/skull.svg" alt="corenominal" class="img-fluid mb-5 invert-light" aria-hidden="true">
+    <img src="/assets/img/skull.svg" alt="corenominal" class="img-152 mb-5 invert-light" aria-hidden="true">
     <h1>Under Construction</h1>
     <p>This page is currently under construction.</p>
 
