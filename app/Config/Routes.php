b5743e6debd82e63763ae8cd16905f4dd0448952
Philip Newborough <corenominal@corenominal.org>
2026-06-05 11:18:42 +0100

    Add unauthorised route and custom 404 override to Routes configuration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,9 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+
+// Unauthorised route
+$routes->get('/unauthorised', 'Unauthorised::index');
+// Custom 404 route
+$routes->set404Override('App\Controllers\Errors::show404');
