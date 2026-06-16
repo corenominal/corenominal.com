1e24c3477a30ce47073097f5921a8085db30ea2d
Philip Newborough <corenominal@corenominal.org>
2026-06-16 15:22:35 +0100

    Fix capitalization in personal description and update site information for clarity

--- app/Views/about.php
+++ app/Views/about.php
@@ -3,7 +3,7 @@
 
     <h1 class="mb-4">About</h1>
 
-    <p>I'm a <?= $age = ((int)date('Y') - 1975) ?> year old tech lover and web developer who spends a lot of time crafting things for the web. When I'm not glued to my desk, you can find me lost in the grimdark future of Warhammer, tackling proud Husband/Dad/Grumpa duties, listening to tech podcasts, or cycling around on my bike.</p>
+    <p>I'm a <?= $age = ((int)date('Y') - 1975) ?> year old tech lover and web developer who spends a lot of time crafting things for the web. When I'm not glued to my desk, you can find me lost in the grimdark future of Warhammer, tackling proud husband/dad/grumpa duties, listening to tech podcasts, or cycling around on my bike.</p>
 
     <p>I've been working in web development since the late 1990s, and have been a professional web developer since 2000. I have a passion for building things that are fast, accessible, and easy to use. I love to learn new things, and I'm always looking for ways to improve my skills.</p>
 
@@ -16,9 +16,11 @@
 
     <p>I currently work full-time as a web developer for a large construction company where I build and maintain their internal web applications.</p>
 
-    <h2>About This Site</h2>
+    <h2 class="mt-5">About This Site</h2>
 
-    <p>This site is built with CodeIgniter, a powerful PHP framework that helps me create robust and scalable web applications. I use it to manage the content and functionality of this site, ensuring it performs well under typical usage scenarios.</p>
+    <p>This site is a personal project of mine, and is a place where I can share my thoughts, ideas, and projects with the world. It's also a place where I can experiment with new technologies and techniques, and learn from my mistakes.</p>
+
+    <p>I've rebuilt this site several times over the years. The current version is built with CodeIgniter, a powerful PHP framework that helps me create robust and scalable web applications. I use it to manage the content and functionality of this site, ensuring it performs well under typical usage scenarios.</p>
 
     <p>The site is hosted on an <a href="https://www.ovhcloud.com/" target="_blank" rel="noopener noreferrer">OVH VPS</a> running <a href="https://www.debian.org/" target="_blank" rel="noopener noreferrer">Debian</a>, with <a href="https://httpd.apache.org/" target="_blank" rel="noopener noreferrer">Apache</a> as the web server and <a href="https://mariadb.org/" target="_blank" rel="noopener noreferrer">MariaDB</a> as the database. I use Git for version control and all the code for this site is stored in a <a href="<?= config('Urls')->git_repository ?>" target="_blank" rel="noopener noreferrer">public Git repository</a>.</p>
 
