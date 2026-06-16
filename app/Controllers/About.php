ca3083560b3b0de090e0dc7206c37886391cf6c7
Philip Newborough <corenominal@corenominal.org>
2026-06-16 15:39:38 +0100

    Remove unused JavaScript and CSS data arrays from About controller

--- app/Controllers/About.php
+++ app/Controllers/About.php
@@ -6,8 +6,6 @@ class About extends BaseController
 {
     public function index()
     {
-        // $data['js']    = [];
-        // $data['css']   = [];
         $data['title'] = 'About';
         return view('about', $data);
     }
