8db68b041a85b71c55e3bcd546b126d71da601f0
Philip Newborough <corenominal@corenominal.org>
2026-06-18 14:20:34 +0100

    Update favicon links to use variables for better flexibility

--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -7,8 +7,8 @@
     <meta name="theme-color" content="#000000">
     <!-- Favicon and touch icons -->
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= $icon32 ?? '/icon-32x32.png' ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= $icon16 ?? '/icon-16x16.png' ?>">
     <?php if (isset($manifest)): ?>
     <!-- Manifest -->
     <link rel="manifest" href="<?= esc($manifest) ?>">
